e3a7455f9c5d6bd69450cab1158ad9d66520a878
Nara <168982560+naragb@users.noreply.github.com>
2026-01-03 11:40:01 -0500

    Update practice.sql

--- scripts/practice.sql
+++ scripts/practice.sql
@@ -4,9 +4,10 @@ SQL Practice
 ============================================================
 
 Note:
-	The dataset for this script not available.
+	The dataset for this script not available. 
+	The purpose of this script is to record the practices on SQL and lessons learned.  
 
- Date:
+ Start date:
 	January 1, 2026
 
 Source:
@@ -68,3 +69,45 @@ CREATE VIEW Sales.V_Order_Details AS(
 
 )
 
+
+/*
+==============================================================================================================================================================================
+Data Lemur: Practice and Lessons 
+==============================================================================================================================================================================
+*/
+
+-- Challenge #1: Teams Power Users
+-- Solution:
+SELECT
+    sender_id,
+    COUNT(message_id) AS message_count
+FROM messages
+WHERE sent_date >= '2022-08-01' AND
+      sent_date < '2022-09-01'
+GROUP BY sender_id
+ORDER BY COUNT(message_id) DESC
+LIMIT 2
+
+/*
+Lesson Learned:
+1. Consider always to put the date in the following format 'YYYY-MM-DD'. Otherwise, the query might not work.
+2. If you are executing ANY calculation in the SELECT query, always think of adding GROUP BY 
+*/
+
+-- Challenge #2: 
+-- Solution:
+SELECT 
+    user_id,
+    MAX(post_date::DATE) - 
+    MIN (post_date::DATE) AS days_between
+FROM posts
+WHERE post_date >= '01/01/2021' AND
+      post_date < '01/01/2022' 
+GROUP BY user_id
+HAVING (MAX(post_date::DATE) - 
+        MIN (post_date::DATE)) != 0
+/*
+Lesson Learned:
+1. If you are asked of subtracting last and first date, consider using MIN() and MAX()
+2. Use GROUP BY, which helps to categorize.
+*/
